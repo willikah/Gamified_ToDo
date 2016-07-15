d0d9deb8452fa98f3e7a74a4eeb4a283da31b1dd
willikah <willi_k@hotmail.de>
2016-07-15 21:16:17 +0200

    Neu: Schickt Namen an Menü (Intent)

--- app/src/main/java/com/apps/kahlmann/gamifiedto_do/CreateChar.java
+++ app/src/main/java/com/apps/kahlmann/gamifiedto_do/CreateChar.java
@@ -1,25 +1,23 @@
 package com.apps.kahlmann.gamifiedto_do;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class CreateChar extends AppCompatActivity {
 
     private static Button start_button;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_char);
 
-
         start_button();
+
     }
 
     //Wenn Startbutton geklickt wird...
@@ -31,15 +29,11 @@ public class CreateChar extends AppCompatActivity {
 
                 //Nimm Text aus Feld
                 EditText name = (EditText)findViewById(R.id.id_Name_field);
-                String name_str = name.getText().toString();
-
-                //schreibe in tview (test)
-                TextView text = (TextView) findViewById(R.id.textView);
-                text.setText(name_str);
 
                 //Starte Menü
-                /*Intent menu_ac = new Intent("com.apps.kahlmann.gamifiedto_do.Menu");
-                startActivity(menu_ac);*/
+                Intent menu_ac = new Intent("com.apps.kahlmann.gamifiedto_do.Menu");
+                menu_ac.putExtra("character_name",name.getText().toString()); // Übergib Textfeld
+                startActivity(menu_ac);
             }
         });
     }
--- app/src/main/java/com/apps/kahlmann/gamifiedto_do/Menu.java
+++ app/src/main/java/com/apps/kahlmann/gamifiedto_do/Menu.java
@@ -1,13 +1,27 @@
 package com.apps.kahlmann.gamifiedto_do;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Menu extends AppCompatActivity {
+
+    private TextView menu_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+
+
+        Intent intent = getIntent();
+
+        // Namen anzeigen
+
+        menu_name = (TextView) findViewById(R.id.menu_name);
+        menu_name.setText("Welcome " + intent.getStringExtra("character_name"));
+
     }
 }
